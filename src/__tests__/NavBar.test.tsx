a206bcb714c58770080b7ae13ff215268f88ae98
Evelin <129849677+EvelinAlvarado@users.noreply.github.com>
2024-10-07 17:47:52 +0100

    Add tests for NavBar component

--- src/__tests__/NavBar.test.tsx
+++ src/__tests__/NavBar.test.tsx
@@ -1,18 +1,18 @@
-import { render, screen } from "@testing-library/react";
 import { NavBar } from "@/components/NavBar";
 import { NAV_LINKS } from "@/constants";
-import { beforeEach } from "node:test";
+import { render, screen } from "@testing-library/react";
 
-describe("NavBar Component", () => {
+describe("NavBar Component at 1024px (desktop view)", () => {
+  beforeEach(() => {
+    render(<NavBar />);
+  });
 
   test("should find the image by its alt text", () => {
-    render(<NavBar />)
     const logoAlt = screen.getByAltText("logo");
     expect(logoAlt).toBeInTheDocument();
   });
 
   test("should render all navigation links", () => {
-    render(<NavBar />)
     NAV_LINKS.forEach((link) => {
       const navLink = screen.getByText(link.label);
       expect(navLink).toBeInTheDocument();
@@ -20,14 +20,18 @@ describe("NavBar Component", () => {
   });
 
   test("should render the Login button", () => {
-    render(<NavBar />)
     const loginButton = screen.getByRole("button", { name: /login/i });
     expect(loginButton).toBeInTheDocument();
   });
 
   test("should render the logo image", () => {
-    render(<NavBar />)
     const menuIcon = screen.getByAltText(/menu/i);
     expect(menuIcon).toBeInTheDocument();
+  });
+
+  test("should render the correct number of navigation links", () => {
+    const navLinks = screen.getAllByRole("link");
+    console.log(navLinks.length);
+    expect(navLinks.length).toBe(6);
   });
 });
